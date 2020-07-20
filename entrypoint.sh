0454a63eddb738b14659479d1d8f4607374ec685
Susmit Vengurlekar <susmit.py@gmail.com>
2020-07-20 16:32:32 +0530

    done

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,9 @@ python manage.py migrate
 python manage.py initadmin
 python manage.py site_config
 
+# AWS EC2 Port 80 to 8000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
+
 # Start server
 echo "Starting server"
 gunicorn resulter.wsgi -b 0.0.0.0:8000
